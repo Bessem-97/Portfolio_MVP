19e2a2ae01475301223b0f112180e67d0d9c7475
Kieran <kieran.webdevelopment@gmail.com>
2022-07-22 08:07:44 +0100

    basic navbar completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div className="nav-right">
         <Link href="/">About me</Link>
         <Link href="/">Resume</Link>
-        <Link href="/">#100DaysofCode</Link>
+        <Link href="/">#100DaysOfCode</Link>
         <Link href="/">Projects</Link>
       </div>
     </nav>
