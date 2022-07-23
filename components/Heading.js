59ef82d968560f13ee6df5d5d65f951af5327dfe
Kieran <kieran.webdevelopment@gmail.com>
2022-07-23 08:01:28 +0100

    header completed

--- components/Heading.js
+++ components/Heading.js
@@ -2,6 +2,14 @@ export default function Heading() {
   return (
     <header>
       <h1>Kieran Gill</h1>
+      <div>
+        <p className="heading-p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          dolore odit esse soluta molestias reiciendis ullam ratione, magni
+          necessitatibus iure inventore beatae, aliquid saepe illum laudantium!
+          Fugit delectus iure ratione.
+        </p>
+      </div>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 // import Image from 'next/image'
+import Heading from '../components/heading';
 import Navbar from '../components/Navbar';
+import Main from '../components/Main';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Heading />
+      <Main />
     </div>
   );
 }
