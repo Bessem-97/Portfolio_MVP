f3dea3601ae20d19a6d8f456f315bea10281fe4e
Kieran <kieran.webdevelopment@gmail.com>
2022-07-23 08:10:07 +0100

    main jsx completed

--- components/Main.js
+++ components/Main.js
@@ -1,3 +1,44 @@
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main className="main-grid-container">
+      <section className="about-me">
+        <h2>About me</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+          facilis impedit ab doloremque necessitatibus architecto fugiat commodi
+          optio voluptatum, debitis, blanditiis quod ducimus iste incidunt,
+          distinctio laborum excepturi! Possimus delectus omnis, doloremque quia
+          nostrum nihil vel eaque accusamus et asperiores iure ab recusandae in
+          debitis, quaerat ut incidunt velit nemo!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure qui quae
+          ad quas aliquam cumque sapiente fugit dolore doloremque quis.
+        </p>
+        <button>View my Resume</button>
+      </section>
+      <div className="main-picture-container">
+        <div className="profile-pics"></div>
+        <div className="profile-pics"></div>
+        <div className="profile-pics"></div>
+        <div className="profile-pics"></div>
+      </div>
+      <section className="100-days">
+        <h2>#100DaysofCode</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          rerum tenetur architecto perferendis vitae! Ipsam possimus architecto,
+          similique autem, eum tempore est iusto dolore obcaecati asperiores
+          dolorem nesciunt soluta ipsa!
+        </p>
+        <button>Read my Diary</button>
+      </section>
+      <section className="toolbox">
+        <h2>My Toolbox</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vitae!
+        </p>
+      </section>
+    </main>
+  );
 }
