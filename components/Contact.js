267d7d20a7d7c7ed01da114251d7caebd94d1a84
Kieran <kieran.webdevelopment@gmail.com>
2022-07-29 18:58:42 +0100

    styled form

--- components/Contact.js
+++ components/Contact.js
@@ -2,24 +2,27 @@ export default function Contact() {
   return (
     <div className="contact">
       <h2>Contact Me </h2>
-      <form>
-        <label htmlFor="">
-          <span>Name</span>
-          <input type="text" />
-        </label>
-        <label htmlFor="">
-          <span>Email</span>
-          <input type="email" />
-        </label>
-        <label htmlFor="">
-          <span>Subject</span>
-          <input type="text" />
-        </label>
-        <label htmlFor="">
-          <span>Subject</span>
-          <textarea />
-        </label>
-      </form>
+      <div className="contact-grid-container">
+        <form className="contact-form">
+          <label htmlFor="Name">Name </label>
+          <input type="text" id="Name" />
+
+          <label htmlFor="Email">Email </label>
+          <input type="text" id="Email" />
+
+          <label htmlFor="Subject">Subject</label>
+
+          <input type="text" id="Subject" />
+
+          <label htmlFor="Message"> Message</label>
+
+          <textarea id="Message" />
+          <button className="btn-main form-button">Submit</button>
+        </form>
+        <section>
+          <h2>Right</h2>
+        </section>
+      </div>
     </div>
   );
 }
