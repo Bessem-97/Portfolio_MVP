7663293935f8452bc4e81e7b1bb8195709873c91
Kieran <kieran.webdevelopment@gmail.com>
2022-07-29 18:04:37 +0100

    added basic form structure

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,24 @@ export default function Contact() {
   return (
     <div className="contact">
       <h2>Contact Me </h2>
+      <form>
+        <label htmlFor="">
+          <span>Name</span>
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          <span>Email</span>
+          <input type="email" />
+        </label>
+        <label htmlFor="">
+          <span>Subject</span>
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          <span>Subject</span>
+          <textarea />
+        </label>
+      </form>
     </div>
   );
 }
