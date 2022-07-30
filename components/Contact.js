040c2e243a082f13eae6f771fea279eb2011f6a8
Kieran <kieran.webdevelopment@gmail.com>
2022-07-30 07:33:40 +0100

    attempt 204

--- components/Contact.js
+++ components/Contact.js
@@ -7,20 +7,19 @@ export default function Contact() {
           className="contact-form"
           name="contact"
           method="POST"
-          data-netlify="true"
+          // data-netlify="true"
         >
           <input type="hidden" name="Contact-me" value="contact" />
-          <label htmlFor="name">Name </label>
-          <input type="text" id="name" name="name" />
+          <label htmlFor="yourname">Name </label>
+          <input type="text" id="yourname" name="name" />
 
-          <label htmlFor="email">Email </label>
-          <input type="text" id="email" name="email" />
+          <label htmlFor="youremail">Email </label>
+          <input type="text" id="youremail" name="email" />
 
-          <label htmlFor="subject">Subject</label>
+          <label htmlFor="thesubject">Subject</label>
+          <input type="text" id="thesubject" name="subject" />
 
-          <input type="text" id="subject" name="subject" />
-
-          <label htmlFor="message">Message</label>
+          <label htmlFor="yourmessage">Message</label>
 
           <textarea id="message" name="message" />
           <button className="btn-main form-button" type="submit">
