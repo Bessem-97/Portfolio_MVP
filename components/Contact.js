4a7ba18aab0030f53f6994dc3d87e7e6a7e315e1
Kieran <kieran.webdevelopment@gmail.com>
2022-07-30 10:26:27 +0100

    fixed possible error in config file

--- components/Contact.js
+++ components/Contact.js
@@ -35,3 +35,4 @@ export default function Contact() {
     </div>
   );
 }
+//test
--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,7 @@ export default function Home() {
       <Heading />
       <Main />
       <Projects />
-      {/* <Contact /> */}
-      <div className="contact">
-        <h2>Contact Me </h2>
-        <div className="contact-grid-container">
-          <form
-            className="contact-form"
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            action="/success"
-            // data-netlify="true"
-          >
-            <input type="hidden" name="contact-me" value="contact" />
-            <label htmlFor="yourname">Name </label>
-            <input type="text" id="yourname" name="name" />
-
-            <label htmlFor="youremail">Email </label>
-            <input type="text" id="youremail" name="email" />
-
-            <label htmlFor="thesubject">Subject</label>
-            <input type="text" id="thesubject" name="subject" />
-
-            <label htmlFor="yourmessage">Message</label>
-
-            <textarea id="yourmessage" name="message" />
-            <button className="btn-main form-button" type="submit">
-              Submit
-            </button>
-          </form>
-          <section>
-            <h2>Right</h2>
-          </section>
-        </div>
-      </div>
+      <Contact />
     </div>
   );
 }
