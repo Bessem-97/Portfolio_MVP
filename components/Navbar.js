7a611a0b34e2578aeabedeed5c6127059ae18588
Kieran <kieran.webdevelopment@gmail.com>
2022-08-17 08:42:20 +0100

    edited cv address

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
           </a>
 
           <a
-            href="https://drive.google.com/file/d/12UdRjUktBZj6KWG0mzpGKRlDVw4G0Zpp/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Kg5LkrDOL_fEDUsl0V1TUa62upXkjl55/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
