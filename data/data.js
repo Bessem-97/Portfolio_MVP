bde724050d6171c1f853781fd412572f7786df0f
Kieran <kieran.webdevelopment@gmail.com>
2023-03-14 15:25:15 +0000

    updated portfolio projects

--- data/data.js
+++ data/data.js
@@ -1,11 +1,14 @@
-import MatchGame from '../public/port pics/match-game.PNG';
-import Recipe from '../public/port pics/recipe.PNG';
-import Bandits from '../public/port pics/Band-its.PNG';
-import StudyBudz from '../public/port pics/studybudz.PNG';
+// import MatchGame from '../public/port pics/match-game.PNG';
+// import Recipe from '../public/port pics/recipe.PNG';
+// import Bandits from '../public/port pics/Band-its.PNG';
+// import StudyBudz from '../public/port pics/studybudz.PNG';
 import Portfolio from '../public/port pics/portfolio-site.PNG';
 import Untrodden from '../public/port pics/untrodden.PNG';
-import eLearning from '../public/port pics/Skilled e-learning landing page.png';
+// import eLearning from '../public/port pics/Skilled e-learning landing page.png';
 import formComp from '../public/port pics/screenshot-form.png';
+import GithubUserSearchApp from '../public/port pics/darkmode screenshot.png';
+import SunnySide from '../public/port pics/sunyside pic.png';
+import timeTrackingDash from '../public/port pics/time tracker.png';
 
 const projects = [
   {
@@ -16,6 +19,30 @@ const projects = [
     linkToSite: `https://untroddenapp.netlify.app/`,
     linkToRepo: `https://github.com/SchoolOfCode/final-project_front-end-untrodden`,
   },
+  {
+    image: GithubUserSearchApp,
+    projectTitle: 'Github User Search App',
+    description:
+      'A React-based web application that allows users to search for GitHub users by username and view their profiles. The app leverages the GitHub API to fetch user data and includes responsive design, error handling, and a dark mode theme feature. ',
+    linkToSite: `https://kieran-gill-github-user-search-app-vite.vercel.app/`,
+    linkToRepo: `https://github.com/KieranWebDev/githubUserSearchApp-vite`,
+  },
+  {
+    image: SunnySide,
+    projectTitle: 'Sunnyside Agency Landing Page',
+    description:
+      'A fully responsive landing page for a fictional agency, built with HTML and CSS; utilising grid and flexbox. I used a Figma design brief from FrontendMentors.io. The aim was to match the original design brief as closely as possible.',
+    linkToSite: `https://kieran-gill-sunny-side-landing-page.vercel.app/`,
+    linkToRepo: `https://github.com/KieranWebDev/FEMentors--SunnySide-landing-Page/tree/main`,
+  },
+  {
+    image: timeTrackingDash,
+    projectTitle: 'Time tracking dashboard',
+    description:
+      'A fully responsive time tracking dashboard for a fictional app. A very simple project that I initially built in Vanilla JS and then rebuilt in React. It was a good stepping stone project, that helped me grasp the fundamentals of React as well as some good practice with CSS grid.',
+    linkToSite: `https://kieran-gill-time-tracking-dashboard-react.vercel.app/`,
+    linkToRepo: `https://github.com/KieranWebDev/time-tracking-dashboard-react`,
+  },
 
   {
     image: Portfolio,
@@ -25,14 +52,14 @@ const projects = [
     linkToSite: `https://kieran-gill-portfolio.netlify.app`,
     linkToRepo: `https://github.com/KieranWebDev/Portfolio-MVP`,
   },
-  {
-    image: eLearning,
-    projectTitle: `E-learning landing Page`,
-    description:
-      'A fully responsive simple landing page for a fictional e-learning site, created with HTML and CSS. I used a figma design brief from FrontendMentors.com. The aim was to match the original design brief as closely as possible',
-    linkToSite: `https://kieran-gill-e-learning-landing-page.vercel.app/`,
-    linkToRepo: `https://github.com/KieranWebDev/FEMentors-eLearningLandingPage/blob/main/README.md`,
-  },
+  // {
+  //   image: eLearning,
+  //   projectTitle: `E-learning landing Page`,
+  //   description:
+  //     'A fully responsive simple landing page for a fictional e-learning site, created with HTML and CSS. I used a figma design brief from FrontendMentors.com. The aim was to match the original design brief as closely as possible',
+  //   linkToSite: `https://kieran-gill-e-learning-landing-page.vercel.app/`,
+  //   linkToRepo: `https://github.com/KieranWebDev/FEMentors-eLearningLandingPage/blob/main/README.md`,
+  // },
   {
     image: formComp,
     projectTitle: `form-component`,
@@ -42,21 +69,21 @@ const projects = [
     linkToRepo: `https://github.com/KieranWebDev/FEMentors-introComponentSignUp`,
   },
 
-  {
-    image: Recipe,
-    projectTitle: 'Recipe Site',
-    description:
-      'A recipe site built with React, Firebase and React Router. The user can create, edit and delete recipes. There is also a dark mode switch which allowed me to learn more about the useContext and useReducer hooks. ',
-    linkToSite: `https://kieran-gill-recipe-site.vercel.app/`,
-    linkToRepo: `https://github.com/KieranWebDev/RecipeDirectory-ReactAndFirebase`,
-  },
-  {
-    image: MatchGame,
-    projectTitle: 'Matching Pairs',
-    description:
-      'A simple pelmanism game I built with React as part of a Udemy course. It helped me to cement some of the React fundamentals and I was also able to learn a few new CSS tricks along the way.',
-    linkToSite: `https://kieran-gill-matching-pairs-game.vercel.app/`,
-    linkToRepo: `https://github.com/KieranWebDev/matching-pairs-game`,
-  },
+  // {
+  //   image: Recipe,
+  //   projectTitle: 'Recipe Site',
+  //   description:
+  //     'A recipe site built with React, Firebase and React Router. The user can create, edit and delete recipes. There is also a dark mode switch which allowed me to learn more about the useContext and useReducer hooks. ',
+  //   linkToSite: `https://kieran-gill-recipe-site.vercel.app/`,
+  //   linkToRepo: `https://github.com/KieranWebDev/RecipeDirectory-ReactAndFirebase`,
+  // },
+  // {
+  //   image: MatchGame,
+  //   projectTitle: 'Matching Pairs',
+  //   description:
+  //     'A simple pelmanism game I built with React as part of a Udemy course. It helped me to cement some of the React fundamentals and I was also able to learn a few new CSS tricks along the way.',
+  //   linkToSite: `https://kieran-gill-matching-pairs-game.vercel.app/`,
+  //   linkToRepo: `https://github.com/KieranWebDev/matching-pairs-game`,
+  // },
 ];
 export default projects;
